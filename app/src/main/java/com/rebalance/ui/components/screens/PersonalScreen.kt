38c9115006951e6e91a9dbb1da1c791be9316d2c
Denys Demianov <demydeni@gmail.com>
2022-12-06 16:51:06 +0100

    Temporary solution

--- app/src/main/java/com/rebalance/ui/components/screens/PersonalScreen.kt
+++ app/src/main/java/com/rebalance/ui/components/screens/PersonalScreen.kt
@@ -4,28 +4,20 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rebalance.R
-import android.util.Log
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.lazy.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.Text
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 
 @Composable
 fun PersonalScreen() {
@@ -42,7 +34,7 @@ fun PersonalScreen() {
         )
     }*/
 
-      // working vertical internal
+    // working vertical internal navigation
     /*Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
 
         // Creating a Scrollable Box
@@ -67,23 +59,85 @@ fun PersonalScreen() {
         }
     }*/
 
-    val listItems: List<ListItem> = listOf(
-        ListItem("Jayme"),
-        ListItem("Gil"),
-        ListItem("Juice WRLD"),
-        ListItem("Callan"),
-        ListItem("Braxton"),
-        ListItem("Kyla"),
-        ListItem("Lil Mosey"),
-        ListItem("Allan"),
-        ListItem("Mike"),
-        ListItem("Drew"),
-        ListItem("Nia"),
-        ListItem("Coi Relay")
-    )
-    DisplayList(listItems)
+    Show()
 }
 
+@Composable
+fun Show() {
+    val listItems: List<ListItem> = listOf(
+        ListItem("Jayme", "Jayme"),
+        ListItem("Gil", "Gil"),
+        ListItem("Juice WRLD", "Juice WRLD"),
+        ListItem("Callan", "Callan"),
+        ListItem("Braxton", "Braxton"),
+        ListItem("Kyla", "Kyla"),
+        ListItem("Lil Mosey", "Lil Mosey"),
+        ListItem("Allan", "Allan"),
+        ListItem("Mike", "Mike"),
+        ListItem("Drew", "Drew"),
+        ListItem("Nia", "Nia"),
+        ListItem("Coi Relay", "Coi Relay")
+    )
+
+    var selectedTabIndex by remember { mutableStateOf(0) }
+
+//    Row(
+//        modifier = Modifier
+//            .fillMaxSize()
+//    ) {
+//        Column(
+//            modifier = Modifier
+//                .fillMaxSize()
+////            .wrapContentSize(Alignment.Center)
+//        ) {
+//            repeat(5) {
+//                Image(
+//                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+//                    contentDescription = "user icon",
+//                    modifier = Modifier
+////                        .padding(horizontal = 8.dp)
+//                )
+//            }
+//        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+//            .wrapContentSize(Alignment.Center)
+        ) {
+            DisplayListHorizontal(listItems, selectedTabIndex) { tabIndex ->
+                selectedTabIndex = tabIndex
+            }
+
+            ListItem(listItems[selectedTabIndex])
+        }
+//    }
+}
+
+@Composable
+fun DisplayListHorizontal(
+    tabs: List<ListItem>,
+    selectedTabIndex: Int,
+    onTabClick: (Int) -> Unit
+) {
+    ScrollableTabRow(
+        selectedTabIndex = selectedTabIndex,
+        edgePadding = 110.dp
+    ) {
+        tabs.forEachIndexed { tabIndex, tab ->
+            Tab(
+                selected = selectedTabIndex == tabIndex,
+                onClick = { onTabClick(tabIndex) },
+                text = { Text(tab.name) },
+                modifier = Modifier
+                    .height(45.dp)
+                    .width(180.dp)
+            )
+        }
+    }
+}
+
+// working 2D navigation
 @Composable
 fun DisplayList(list: List<ListItem>) {
     LazyRow(modifier = Modifier.fillMaxHeight()) {
@@ -130,4 +184,11 @@ fun ListItem(item: ListItem) {
     }
 }
 
-data class ListItem(val name: String)
+data class ListItem(val name: String, val text: String)
+
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    Show()
+}
