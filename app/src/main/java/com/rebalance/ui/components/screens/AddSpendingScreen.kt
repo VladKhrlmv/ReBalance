56f3d45570018d997b28a18f90d20c1c538d1c9e
Slovjaninsky <nevage7go@gmail.com>
2022-12-17 23:41:14 +0100

    Start implementation of AddSpendingScreen

--- app/src/main/java/com/rebalance/ui/components/screens/AddSpendingScreen.kt
+++ app/src/main/java/com/rebalance/ui/components/screens/AddSpendingScreen.kt
@@ -1,27 +1,99 @@
 package com.rebalance.ui.components.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import android.graphics.Typeface
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+val costValueRegex = """^\d{0,12}[\.\,]{0,1}\d{0,2}${'$'}""".toRegex()
 
 @Composable
 fun AddSpendingScreen() {
+    var spendingName by remember { mutableStateOf(TextFieldValue()) }
+    var expandedDropdownCategory by remember { mutableStateOf(false) }
+    var selectedCategory by remember { mutableStateOf("") }
+    var costValue by remember { mutableStateOf(TextFieldValue()) }
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
+            .wrapContentSize(Alignment.TopStart)
+            .padding(10.dp)
     ) {
         Text(
-            text = "Add spending View",
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp
+            text = "Add spending",
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(10.dp),
+            fontFamily = FontFamily(Typeface.DEFAULT),
+            fontSize = 32.sp
         )
+        Row {
+            TextField(
+                value = spendingName,
+                onValueChange = { newSpendingName -> spendingName = newSpendingName },
+                placeholder = { Text(text = "Title") },
+                modifier = Modifier
+                    .padding(10.dp)
+            )
+            Box (contentAlignment = Alignment.Center) {
+                IconButton(onClick = { expandedDropdownCategory = true } ) {
+                    Row {
+                        Icon(
+                            imageVector = if (expandedDropdownCategory) Icons.Rounded.ArrowDropUp else Icons.Rounded.ArrowDropDown,
+                            contentDescription = "Open categories",
+                            modifier = Modifier
+                                .size(64.dp)
+                        )
+                    }
+                }
+                DropdownMenu(expanded = expandedDropdownCategory, onDismissRequest = { expandedDropdownCategory = false }) {
+                    DropdownMenuItem(onClick = { selectedCategory = "Sport" }) {
+                        // icon can be placed before text
+                        Text(text = "Sport")
+                    }
+                    DropdownMenuItem(onClick = { selectedCategory = "Clothing" }) {
+                        Text(text = "Clothing")
+                    }
+                }
+            }
+        }
+        Row {
+            TextField(
+                value = costValue,
+                onValueChange = { newCostValue ->
+                    if (costValueRegex.matches(newCostValue.text)) {
+                        costValue = newCostValue
+                    }
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                placeholder = { Text(text = "0.00") },
+                modifier = Modifier
+                    .padding(10.dp)
+                    .onFocusChanged {
+                        if (!it.isFocused) {
+                            var tempCostValue = costValue.text
+                                .replace(",", ".")
+                                .replace("""^\.""".toRegex(), "0.")
+                                .replace("""\.$""".toRegex(), ".00");
+                            costValue = TextFieldValue(tempCostValue)
+                        }
+                    }
+            )
+
+        }
+        Row {
+
+        }
     }
 }
