1f62cfa0830c6c2cd05589c58133c1fb84dc97f3
Slovjaninsky <nevage7go@gmail.com>
2022-12-18 16:50:54 +0100

    First working version of DateField

--- app/src/main/java/com/rebalance/ui/components/DateField.kt
+++ app/src/main/java/com/rebalance/ui/components/DateField.kt
@@ -1,11 +1,18 @@
 package com.rebalance.ui.components
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.vanpra.composematerialdialogs.MaterialDialog
@@ -14,8 +21,10 @@ import com.vanpra.composematerialdialogs.rememberMaterialDialogState
 
 @Composable
 fun DatePickerField() {
-    val textState = remember { mutableStateOf(TextFieldValue()) }
-    var dateDialog = MaterialDialog(
+    var textState = remember { mutableStateOf(TextFieldValue()) }
+    var dialogState = rememberMaterialDialogState()
+    MaterialDialog(
+        dialogState = dialogState,
         buttons = {
             positiveButton("Ok")
             negativeButton("Cancel")
@@ -25,7 +34,23 @@ fun DatePickerField() {
             textState.value = TextFieldValue(date.toString())
         }
     }
-    Column(modifier = Modifier.padding(16.dp)) {
-
+    Column(modifier = Modifier.padding(10.dp)) {
+        Box {
+            TextField(
+                value = textState.value,
+                onValueChange = { textState.value = it },
+                enabled = true,
+                readOnly = true,
+                label = {
+                    Text(text = "Date")
+                }
+            )
+            Box(
+                modifier = Modifier
+                    .matchParentSize()
+                    .alpha(0f)
+                    .clickable { dialogState.show() }
+            )
+        }
     }
 }
