9e6c69ac99037ff7c01053715af24493beb92d15
Slovjaninsky <nevage7go@gmail.com>
2022-12-18 22:41:55 +0100

    Recreated DatePicker without using external library

--- app/src/main/java/com/rebalance/ui/components/DateField.kt
+++ app/src/main/java/com/rebalance/ui/components/DateField.kt
@@ -1,7 +1,6 @@
 package com.rebalance.ui.components
 
-import android.os.Build
-import androidx.annotation.RequiresApi
+import android.app.DatePickerDialog
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,32 +12,41 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.vanpra.composematerialdialogs.MaterialDialog
-import com.vanpra.composematerialdialogs.datetime.date.datepicker
-import com.vanpra.composematerialdialogs.rememberMaterialDialogState
+import android.widget.DatePicker
+import java.util.Calendar
+import java.util.Date
 
 @Composable
 fun DatePickerField() {
-    var textState = remember { mutableStateOf(TextFieldValue()) }
-    var dialogState = rememberMaterialDialogState()
-    MaterialDialog(
-        dialogState = dialogState,
-        buttons = {
-            positiveButton("Ok")
-            negativeButton("Cancel")
-        }
-    ) {
-        datepicker { date ->
-            textState.value = TextFieldValue(date.toString())
-        }
-    }
+    val mContext = LocalContext.current
+
+    val mYear: Int
+    val mMonth: Int
+    val mDay: Int
+
+    val mCalendar = Calendar.getInstance()
+
+    mYear = mCalendar.get(Calendar.YEAR)
+    mMonth = mCalendar.get(Calendar.MONTH)
+    mDay = mCalendar.get(Calendar.DAY_OF_MONTH)
+
+    mCalendar.time = Date()
+
+    val mDate = remember { mutableStateOf("") }
+
+    val mDatePickerDialog = DatePickerDialog(
+        mContext,
+        { _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
+            mDate.value = "$mDayOfMonth/${mMonth+1}/$mYear"
+        }, mYear, mMonth, mDay
+    )
     Column(modifier = Modifier.padding(10.dp)) {
         Box {
             TextField(
-                value = textState.value,
-                onValueChange = { textState.value = it },
+                value = mDate.value,
+                onValueChange = {  },
                 enabled = true,
                 readOnly = true,
                 label = {
@@ -49,7 +57,7 @@ fun DatePickerField() {
                 modifier = Modifier
                     .matchParentSize()
                     .alpha(0f)
-                    .clickable { dialogState.show() }
+                    .clickable { mDatePickerDialog.show() }
             )
         }
     }
