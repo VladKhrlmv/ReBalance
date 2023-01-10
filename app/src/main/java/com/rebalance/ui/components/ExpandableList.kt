9acc20bea93c877e309b1368d7921566d059f2ef
Denys Demianov <demydeni@gmail.com>
2023-01-10 13:01:59 +0100

    Add list view to Personal screen

--- app/src/main/java/com/rebalance/ui/components/ExpandableList.kt
+++ app/src/main/java/com/rebalance/ui/components/ExpandableList.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,11 +16,9 @@ import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import java.util.Date
+import com.rebalance.backend.service.ExpenseItem
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -34,7 +31,7 @@ fun ExpandableList(items: List<ExpenseItem>) {
             val expanded = remember{ mutableStateOf(false) }
             ListItem(
                 text = { Text(item.category) },
-                icon = {
+                icon = { //TODO: change to category icon
                     CardArrow(expanded.value)
                 },
                 trailing = {
@@ -65,13 +62,13 @@ fun ExpandableList(items: List<ExpenseItem>) {
                         .padding(16.dp)
                         .fillMaxWidth(), contentAlignment = Alignment.TopStart) {
                     Surface(color = Color.hsl(103f, 0f, 0.95f)) {
-                        Column(Modifier
+                        Column(Modifier //TODO: change to LazyColumn
                             .padding(16.dp)
                             .fillMaxWidth()) {
                             for (expense in item.expenses) {
                                 Row(modifier = Modifier.wrapContentSize().fillMaxWidth().padding(16.dp), horizontalArrangement = Arrangement.SpaceBetween) {
-                                    Text(expense.dateStamp.toString(), style = MaterialTheme.typography.subtitle1)
-                                    Text(expense.amount.toString(), style = MaterialTheme.typography.subtitle1)
+                                    Text(expense.getDateStamp(), style = MaterialTheme.typography.subtitle1)
+                                    Text(expense.getAmount().toString(), style = MaterialTheme.typography.subtitle1)
                                 }
                             }
                         }
@@ -88,7 +85,7 @@ fun CardArrow(
     bool: Boolean,
 ) {
     Icon(
-        Icons.Default.ArrowDropDown,
+        Icons.Default.ArrowDropDown, //TODO: change to different icons
         contentDescription = "Expandable Arrow",
         modifier = Modifier.rotate(if (bool) 180f else 0f).size(45.dp)
     )
