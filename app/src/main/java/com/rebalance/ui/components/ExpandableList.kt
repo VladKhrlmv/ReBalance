4a82ad7d3126b781256cc8eacfac6d611702cdc9
Uladzislau Kharlamau <kharlamov-vlad03@mail.ru>
2023-01-10 01:40:20 +0100

    Expandable logic completed

--- app/src/main/java/com/rebalance/ui/components/ExpandableList.kt
+++ app/src/main/java/com/rebalance/ui/components/ExpandableList.kt
@@ -1,209 +1,112 @@
 package com.rebalance.ui.components
 
-import android.annotation.SuppressLint
-import androidx.compose.animation.*
-import androidx.compose.animation.core.*
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import androidx.compose.ui.unit.sp
+import java.util.Date
 
-data class ExpandableCardModel(val id: Int, val title: String)
 
-class CardsViewModel : ViewModel() {
-    private val _cards = MutableStateFlow(listOf<ExpandableCardModel>())
-    val cards: StateFlow<List<ExpandableCardModel>> get() = _cards
-
-    private val _expandedCardIdsList = MutableStateFlow(listOf<Int>())
-    val expandedCardIdsList: StateFlow<List<Int>> get() = _expandedCardIdsList
-
-    init {
-        getFakeData()
-    }
-
-    private fun getFakeData() {
-        viewModelScope.launch {
-            withContext(Dispatchers.Default) {
-                val testList = arrayListOf<ExpandableCardModel>()
-                repeat(20) { testList += ExpandableCardModel(id = it, title = "Card $it") }
-                _cards.emit(testList)
-            }
-        }
-    }
-
-    fun onCardArrowClicked(cardId: Int) {
-        _expandedCardIdsList.value = _expandedCardIdsList.value.toMutableList().also { list ->
-            if (list.contains(cardId)) list.remove(cardId) else list.add(cardId)
-        }
-    }
+class PersonalCategory(name: String, items: List<CategoryItem>, total: Float) {
+    var name = name
+    var items= items
+    var total = total
+}
+class CategoryItem(date: String, price: Float) {
+    var date = date
+    var price = price
 }
 
+// items: List<PersonalCategory>
+
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun CardsScreen(viewModel: CardsViewModel) {
-    val cards by viewModel.cards.collectAsState()
-    val expandedCardIds by viewModel.expandedCardIdsList.collectAsState()
-
-    Scaffold {
-        LazyColumn {
-            this.items(cards, ExpandableCardModel::id) { card ->
-                ExpandableCard(
-                    card = card,
-                    onCardArrowClick = { viewModel.onCardArrowClicked(card.id) },
-                    expanded = expandedCardIds.contains(card.id),
-                )
-            }
-        }
-    }
-}
-
-@SuppressLint("UnusedTransitionTargetStateParameter")
-@Composable
-fun ExpandableCard(
-    card: ExpandableCardModel,
-    onCardArrowClick: () -> Unit,
-    expanded: Boolean,
-) {
-    val transitionState = remember {
-        MutableTransitionState(expanded).apply {
-            targetState = !expanded
-        }
-    }
-    val transition = updateTransition(transitionState)
-
-    val cardPaddingHorizontal by transition.animateDp({
-        tween(durationMillis = 1000)
-    }) {
-        if (expanded) 48.dp else 24.dp
-    }
-
-    val cardElevation by transition.animateDp({
-        tween(durationMillis = 1000)
-    }) {
-        if (expanded) 24.dp else 4.dp
-    }
-    val cardRoundedCorners by transition.animateDp({
-        tween(
-            durationMillis = 1000,
-            easing = FastOutSlowInEasing
-        )
-    }) {
-        if (expanded) 0.dp else 16.dp
-    }
-    val arrowRotationDegree by transition.animateFloat({
-        tween(durationMillis = 1000)
-    }) {
-        if (expanded) 0f else 180f
-    }
-
-    Card(
-        elevation = cardElevation,
-        shape = RoundedCornerShape(cardRoundedCorners),
+fun ExpandableList() {
+    val items = listOf<PersonalCategory>(PersonalCategory("first", listOf<CategoryItem>(CategoryItem("1/1/2000", 12.3f), CategoryItem("1/1/2000", 12.3f), CategoryItem("1/1/2000", 12.3f)), 36.9f),
+        PersonalCategory("second", listOf<CategoryItem>(CategoryItem("1/1/2000", 12.3f)), 12.3f),
+        PersonalCategory("third", listOf<CategoryItem>(CategoryItem("1/1/2000", 12.3f)), 12.3f),
+        PersonalCategory("fourth", listOf<CategoryItem>(CategoryItem("1/1/2000", 12.3f)), 12.3f))
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(
-                horizontal = cardPaddingHorizontal,
-                vertical = 8.dp
+            .verticalScroll(rememberScrollState())
+    )  {
+        for (category in items) {
+            val expanded = remember{ mutableStateOf(false) }
+            ListItem(
+                text = { Text(category.name) },
+                icon = {
+                    CardArrow(expanded.value)
+                },
+                trailing = {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(text = category.total.toString() + " PLN", fontSize = 14.sp, color = Color.hsl(358f, 0.63f, 0.49f))
+                    CardArrow(expanded.value)}
+                           },
+                modifier = Modifier
+                    .clickable {
+                        expanded.value = !expanded.value
+                    }
+                    .drawBehind {
+                        val strokeWidth = 1 * density
+                        val y = size.height - strokeWidth / 2
+
+                        drawLine(
+                            Color.LightGray,
+                            Offset(0f, y),
+                            Offset(size.width, y),
+                            strokeWidth
+                        )
+                    }
             )
-    ) {
-        Column {
-            Box {
-                CardArrow(
-                    degrees = arrowRotationDegree,
-                    onClick = onCardArrowClick
-                )
-                CardTitle(title = card.title)
+
+            if (expanded.value) {
+                Box(
+                    Modifier
+                        .padding(16.dp)
+                        .fillMaxWidth(), contentAlignment = Alignment.TopStart) {
+                    Surface(color = Color.hsl(103f, 0f, 0.95f)) {
+                        Column(Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth()) {
+                            for (item in category.items) {
+                                Row(modifier = Modifier.wrapContentSize().fillMaxWidth().padding(16.dp), horizontalArrangement = Arrangement.SpaceBetween) {
+                                    Text(item.date.toString(), style = MaterialTheme.typography.subtitle1)
+                                    Text(item.price.toString(), style = MaterialTheme.typography.subtitle1)
+                                }
+                            }
+                        }
+                    }
+                }
             }
-            ExpandableContent(visible = expanded, initialVisibility = expanded)
         }
     }
-}
-
-@Composable
-fun CardTitle(title: String) {
-    Text(
-        text = title,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        textAlign = TextAlign.Center,
-    )
 }
 
 
 @Composable
 fun CardArrow(
-    degrees: Float,
-    onClick: () -> Unit
+    bool: Boolean,
 ) {
-    IconButton(
-        onClick = onClick,
-        content = {
-            Icon(
-                Icons.Default.MoreVert,
-                contentDescription = "Expandable Arrow",
-                modifier = Modifier.rotate(degrees),
-            )
-        }
+    Icon(
+        Icons.Default.ArrowDropDown,
+        contentDescription = "Expandable Arrow",
+        modifier = Modifier.rotate(if (bool) 180f else 0f).size(45.dp)
     )
-}
-
-@OptIn(ExperimentalAnimationApi::class)
-@Composable
-fun ExpandableContent(
-    visible: Boolean = true,
-    initialVisibility: Boolean = false
-) {
-    val enterTransition = remember {
-        expandVertically(
-            expandFrom = Alignment.Top,
-            animationSpec = tween(1000)
-        ) + fadeIn(
-            initialAlpha = 0.3f,
-            animationSpec = tween(1000)
-        )
-    }
-    val exitTransition = remember {
-        shrinkVertically(
-            // Expand from the top.
-            shrinkTowards = Alignment.Top,
-            animationSpec = tween(1000)
-        ) + fadeOut(
-            // Fade in with the initial alpha of 0.3f.
-            animationSpec = tween(1000)
-        )
-    }
-    AnimatedVisibility(
-        visible = visible,
-        initiallyVisible = initialVisibility,
-        enter = enterTransition,
-        exit = exitTransition
-    ) {
-        Column(modifier = Modifier.padding(8.dp)) {
-            Spacer(modifier = Modifier.heightIn(100.dp))
-            Text(
-                text = "Expandable content here",
-                textAlign = TextAlign.Center
-            )
-        }
-
-    }
 }
