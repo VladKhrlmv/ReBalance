29672192f693b6f3fa6ad539a7159bf165a5a34c
Aliaksei Savich <lesha.savich1@gmail.com>
2023-01-10 01:04:26 +0100

    Connect pie chart to data from server

--- app/src/main/java/com/rebalance/ui/components/PieChart.kt
+++ app/src/main/java/com/rebalance/ui/components/PieChart.kt
@@ -2,6 +2,8 @@ package com.rebalance.ui.components
 
 import android.graphics.Typeface
 import android.os.Build
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.annotation.RequiresApi
@@ -19,44 +21,41 @@ import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.ValueFormatter
-import com.google.gson.Gson
 import com.rebalance.R
 import com.rebalance.backend.GlobalVars
 import com.rebalance.backend.api.jsonArrayToExpenses
 import com.rebalance.backend.api.sendGet
-import com.rebalance.backend.api.sendPost
 import com.rebalance.backend.entities.Expense
 import com.rebalance.ui.theme.categoryColor
 import com.rebalance.ui.theme.greyColor
 import com.rebalance.ui.theme.redColor
 import java.text.DecimalFormat
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+
 
 data class PieChartData(var category: String, var value: Double)
 
 @RequiresApi(Build.VERSION_CODES.N)
 fun getPieChartData(): ArrayList<PieChartData> {
+    val policy = ThreadPolicy.Builder().permitAll().build()
+    StrictMode.setThreadPolicy(policy)
     var entries = ArrayList<PieChartData>()
-    Thread {
-        try {
-            var jsonBodyGet = sendGet(
-                "http://${GlobalVars().getIp()}/groups/2/expenses"
-            )
-            var listExpense: List<Expense> = jsonArrayToExpenses(jsonBodyGet)
-            println(jsonBodyGet)
-            listExpense.forEach { entry ->
-                entries.add(
-                    PieChartData(
-                        entry.getCategory(),
-                        entry.getAmount().toDouble()
-                    )
-                )
-            }
-        } catch (e: Exception) {
-            print(e.stackTrace)
+
+    var jsonBodyGet = sendGet(
+        "http://${GlobalVars().getIp()}/groups/2/expenses"
+    )
+    var listExpense: List<Expense> = jsonArrayToExpenses(jsonBodyGet)
+    println(listExpense)
+    var categoryMap: HashMap<String, Int> = HashMap()
+    listExpense.forEach { entry ->
+        if (categoryMap.containsKey(entry.getCategory())) {
+            categoryMap[entry.getCategory()] = categoryMap.getValue(entry.getCategory()) + entry.getAmount()
+        } else {
+            categoryMap[entry.getCategory()] = entry.getAmount()
         }
-    }.start()
+    }
+    for (entry in categoryMap.entries.iterator()) {
+        entries.add(PieChartData(entry.key, entry.value.toDouble()))
+    }
     //todo https://stackoverflow.com/questions/6343166/how-can-i-fix-android-os-networkonmainthreadexception#:~:text=Implementation%20summary
     return entries
 }
@@ -111,7 +110,7 @@ fun updatePieChartWithData(
     }
 
     val ds = PieDataSet(entries, "")
-    ds.colors = data.map { categoryColor[it.category]?.toArgb() ?: greyColor.toArgb() }
+//    ds.colors = data.map { categoryColor[it.category]?.toArgb() ?: greyColor.toArgb() }
     ds.yValuePosition = PieDataSet.ValuePosition.INSIDE_SLICE
     ds.xValuePosition = PieDataSet.ValuePosition.INSIDE_SLICE
     ds.valueFormatter = object : ValueFormatter() {
@@ -121,22 +120,22 @@ fun updatePieChartWithData(
         }
     }
     ds.sliceSpace = 2F
-    ds.setValueTextColors(data.map {
-        categoryColor[it.category]?.red?.let { red ->
-            categoryColor[it.category]?.blue?.let { blue ->
-                categoryColor[it.category]?.green?.let { green ->
-                    categoryColor[it.category]?.alpha?.let { alpha ->
-                        Color(
-                            red = 1f - red,
-                            green = 1f - green,
-                            blue = 1f - blue,
-                            alpha = alpha
-                        ).toArgb()
-                    }
-                }
-            }
-        }
-    })
+//    ds.setValueTextColors(data.map {
+//        categoryColor[it.category]?.red?.let { red ->
+//            categoryColor[it.category]?.blue?.let { blue ->
+//                categoryColor[it.category]?.green?.let { green ->
+//                    categoryColor[it.category]?.alpha?.let { alpha ->
+//                        Color(
+//                            red = 1f - red,
+//                            green = 1f - green,
+//                            blue = 1f - blue,
+//                            alpha = alpha
+//                        ).toArgb()
+//                    }
+//                }
+//            }
+//        }
+//    })
     ds.valueTextSize = 18f
     ds.valueTypeface = Typeface.DEFAULT_BOLD
     val d = PieData(ds)
