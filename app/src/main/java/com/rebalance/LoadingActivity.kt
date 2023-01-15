84f8dfbf0ccfaaaaf52a539874ab760ab6597ebc
Denys Demianov <demydeni@gmail.com>
2023-01-15 04:36:07 +0100

    Add checking for connection

--- app/src/main/java/com/rebalance/LoadingActivity.kt
+++ app/src/main/java/com/rebalance/LoadingActivity.kt
@@ -2,6 +2,8 @@ package com.rebalance
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -16,8 +18,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
-import com.rebalance.backend.api.sendGet
 import com.rebalance.ui.theme.ReBalanceTheme
+import java.net.HttpURLConnection
+import java.net.URL
 
 class LoadingActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,10 +40,15 @@ fun LoadingScreen() {
     val context = LocalContext.current
     val preferences = Preferences(LocalContext.current).read()
     val isLoading = remember { mutableStateOf(true) }
+
+    val connected = remember { mutableStateOf(0) }
+
     Scaffold(
         content = { padding -> // We have to pass the scaffold inner padding to our content. That's why we use Box.
             Column(
-                modifier = Modifier.fillMaxSize().padding(padding),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             )  {
@@ -51,15 +59,19 @@ fun LoadingScreen() {
             }
         }
     )
-    try {
-        sendGet("http://${preferences.serverIp}/connect")
+
+    tryConnect(preferences) {
+        connected.value = it
+    }
+
+    if (connected.value == 1) {
         if (!preferences.exists()) {
             context.startActivity(Intent(context, SignInActivity::class.java))
         } else {
             context.startActivity(Intent(context, MainActivity::class.java))
         }
-        isLoading.value = false
-    } catch (exception: Exception) {
+    }
+    else if (connected.value == -1) {
         ContextCompat.getMainExecutor(context).execute {
             Toast.makeText(
                 context,
@@ -68,6 +80,27 @@ fun LoadingScreen() {
             ).show()
         }
     }
+}
 
+fun tryConnect(
+    preferences: PreferencesData,
+    onConnect: (Int) -> Unit
+) {
+    val mainLooper = Looper.getMainLooper()
 
+    Thread {
+        try {
+            val imageUrl = URL("http://${preferences.serverIp}/connect/test")
+
+            val httpConnection = imageUrl.openConnection() as HttpURLConnection
+            httpConnection.doInput = true
+            httpConnection.connect()
+
+            Handler(mainLooper).post {
+                onConnect(1)
+            }
+        } catch (e: Exception) {
+            onConnect(-1)
+        }
+    }.start()
 }
