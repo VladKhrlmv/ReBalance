b6b67d5958b0ec9e965f3a8d983b9388249d08cb
Aliaksei Savich <lesha.savich1@gmail.com>
2023-04-28 18:23:05 +0200

    Change exception policy

--- app/src/main/java/com/rebalance/Preferences.kt
+++ app/src/main/java/com/rebalance/Preferences.kt
@@ -13,8 +13,8 @@ class Preferences(
     private val gson = Gson()
     private val fileName = "preferences.txt"
 //    private val serverIp = "156.17.239.158:8080" // Denis' dorm IP
-    private val serverIp = "156.17.239.187:8080" // Alexei's dorm IP
-//    private val serverIp = "192.168.62.115:8080" // Uni hotspot IP
+//    private val serverIp = "156.17.239.187:8080" // Alexei's dorm IP
+    private val serverIp = "192.168.62.115:8080" // Uni hotspot IP
 
     fun read(): PreferencesData {
         return try {
--- app/src/main/java/com/rebalance/backend/api/RequestsSender.kt
+++ app/src/main/java/com/rebalance/backend/api/RequestsSender.kt
@@ -13,7 +13,7 @@ class RequestsSender {
             with(url.openConnection() as HttpURLConnection) {
                 requestMethod = "GET"
                 println("Sent 'GET' request to URL : $url; Response Code : $responseCode")
-                if (responseCode == 409 || responseCode == 400) {
+                if (responseCode == 409 || responseCode == 400 || responseCode == 204) {
                     throw ServerException()
                 }
                 if(responseCode == 500){
