955d4d7fcd1fd5aa950d27acc89f5fc52f43c8d7
Aliaksei Savich <lesha.savich1@gmail.com>
2023-05-31 11:06:13 +0200

    Disable 'back' button for the whole application

--- app/src/main/java/com/rebalance/ui/components/screens/navigation/ScreenNavigation.kt
+++ app/src/main/java/com/rebalance/ui/components/screens/navigation/ScreenNavigation.kt
@@ -1,6 +1,7 @@
 package com.rebalance.ui.components.screens.navigation
 
 import android.content.Context
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -21,30 +22,57 @@ fun ScreenNavigation(
     NavHost(navController, startDestination = startDestination) {
         composable(ScreenNavigationItem.Personal.route) {
             PersonalScreen(context, pieChart)
+            BackHandler(true) {
+                // do nothing
+            }
         }
         composable(ScreenNavigationItem.Group.route) {
             GroupScreen(context)
+            BackHandler(true) {
+                // do nothing
+            }
         }
         composable(ScreenNavigationItem.AddSpending.route) {
             AddSpendingScreen(context)
+            BackHandler(true) {
+                // do nothing
+            }
         }
         composable(ScreenNavigationItem.SignIn.route) {
             SignInScreen(context, navController)
+            BackHandler(true) {
+                // do nothing
+            }
         }
         composable(ScreenNavigationItem.SignUp.route) {
             SignUpScreen(navController)
+            BackHandler(true) {
+                // do nothing
+            }
         }
         composable(ScreenNavigationItem.SignUpMail.route) {
             SignUpMailScreen(context, navController)
+            BackHandler(true) {
+                // do nothing
+            }
         }
         composable("mainActivity") {
             MainActivity()
+            BackHandler(true) {
+                // do nothing
+            }
         }
         composable("signInActivity") {
             SignInActivity()
+            BackHandler(true) {
+                // do nothing
+            }
         }
         composable(ScreenNavigationItem.Settings.route) {
             SettingsScreen(navController)
+            BackHandler(true) {
+                // do nothing
+            }
         }
     }
 }
