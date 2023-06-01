d001c024873636cf53d52974d707e758d93528cd
DemyDeni <demydeni@gmail.com>
2023-06-01 20:15:38 +0200

    Rename SignInActivity to AuthenticationActivity

---
+++ app/src/main/java/com/rebalance/activity/AuthenticationActivity.kt
@@ -0,0 +1,19 @@
+package com.rebalance.activity
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.rebalance.ui.screen.signin.AuthenticationScreen
+import com.rebalance.ui.theme.ReBalanceTheme
+
+
+class AuthenticationActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ReBalanceTheme {
+                AuthenticationScreen()
+            }
+        }
+    }
+}
