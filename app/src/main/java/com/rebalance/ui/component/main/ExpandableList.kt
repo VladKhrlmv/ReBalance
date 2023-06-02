98302af170477f2d729d822645f5ce8121ba6e32
DemyDeni <demydeni@gmail.com>
2023-06-02 18:05:30 +0200

    Fix personal screen list view colors

--- app/src/main/java/com/rebalance/ui/component/main/ExpandableList.kt
+++ app/src/main/java/com/rebalance/ui/component/main/ExpandableList.kt
@@ -12,9 +12,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
@@ -42,7 +40,6 @@ fun ExpandableList(
             ListItem(
                 headlineContent = { Text(item.text) },
                 leadingContent = { //TODO: change to category icon
-                    CardArrow(expanded.value)
                 },
                 trailingContent = {
                     Row(verticalAlignment = Alignment.CenterVertically) {
@@ -58,17 +55,6 @@ fun ExpandableList(
                     .clickable {
                         expanded.value = !expanded.value
                     }
-                    .drawBehind {
-                        val strokeWidth = 1 * density
-                        val y = size.height - strokeWidth / 2
-
-                        drawLine(
-                            Color.LightGray,
-                            Offset(0f, y),
-                            Offset(size.width, y),
-                            strokeWidth
-                        )
-                    }
             )
 
             if (expanded.value) {
@@ -77,7 +63,7 @@ fun ExpandableList(
                         .padding(16.dp)
                         .fillMaxWidth(), contentAlignment = Alignment.TopStart
                 ) {
-                    Surface(color = Color.hsl(103f, 0f, 0.95f)) {
+                    Surface() {
                         Column(
                             Modifier //TODO: change to LazyColumn
                                 .padding(15.dp)
@@ -99,7 +85,6 @@ fun ExpandableList(
                                         expense.getAmount().toString(),
                                         style = MaterialTheme.typography.titleMedium
                                     )
-//TODO: fix
                                     Column(
                                         modifier = Modifier
                                             .wrapContentSize()
