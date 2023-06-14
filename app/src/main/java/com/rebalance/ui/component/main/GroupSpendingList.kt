ce41bf0f310b30fecfdd33e3ea16706cfefe060c
Uladzislau Kharlamau <kharlamov-vlad03@mail.ru>
2023-06-15 00:03:01 +0200

    Update group spending list

--- app/src/main/java/com/rebalance/ui/component/main/GroupSpendingList.kt
+++ app/src/main/java/com/rebalance/ui/component/main/GroupSpendingList.kt
@@ -4,15 +4,23 @@ import android.content.Context
 import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rebalance.PreferencesData
@@ -21,6 +29,7 @@ import com.rebalance.backend.service.BackendService
 import com.rebalance.utils.alertUser
 import compose.icons.EvaIcons
 import compose.icons.evaicons.Fill
+import compose.icons.evaicons.fill.Image
 import compose.icons.evaicons.fill.Trash
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -37,6 +46,7 @@ fun GroupSpendingList(
     val groupCurrency =
         if (groupId == -1L) "" else BackendService(preferences).getGroupById(groupId).getCurrency()
 
+
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -44,166 +54,192 @@ fun GroupSpendingList(
         verticalArrangement = Arrangement.Top
     ) {
         val expensesByGlobalId: Map<Long?, List<Expense>> = data.groupBy { it.getGlobalId() }
+        val filteredData = data.filter { it.getAmount() >= 0 }
         items(
-            items = data.filter { it.getAmount() >= 0 },
-            itemContent = { item -> //TODO: apply filter previously
-                Box(
+            items = filteredData,
+            itemContent = { expense -> //TODO: apply filter previously
+                val expanded = remember { mutableStateOf(false) }
+                Card(
                     modifier = Modifier
-                        .fillMaxWidth()
                         .padding(10.dp)
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    shape = MaterialTheme.shapes.medium,
+                    elevation = CardDefaults.cardElevation(8.dp),
+                    colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surface)
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(Color.LightGray)
-                    ) {
-                        Row(
-                            modifier = Modifier
-                                .padding(10.dp)
-                                .fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ) {
-                            Text(
-                                text = item.getDescription(),
-                                fontSize = 14.sp,
+                    ListItem(
+                        headlineContent = {
+                            Row(
                                 modifier = Modifier
-                                    .padding(5.dp)
-                            )
-                            Text(
-                                text = "${item.getAmount()} $groupCurrency",
-                                fontSize = 14.sp,
-                                color = Color.hsl(358f, 0.63f, 0.49f),
-                                modifier = Modifier
-                                    .padding(5.dp)
-                            )
+                                    .fillMaxSize(),
+                                verticalAlignment = Alignment.Bottom
+                            ) {
 
-                            val showDialog = remember { mutableStateOf(false) }
+                                val text = buildAnnotatedString {
 
-                            IconButton(onClick = {
-                                showDialog.value = true
-                            }) {
-                                Icon(EvaIcons.Fill.Trash, "Delete expense")
-                            }
-                            if (showDialog.value) {
-                                AlertDialog(
-                                    onDismissRequest = { showDialog.value = false },
-                                    title = { Text("Confirmation") },
-                                    text = { Text("Are you sure you want to delete this expense?") },
-                                    confirmButton = {
-                                        TextButton(onClick = {
-                                            BackendService(preferences).deleteExpenseByGlobalId(
-                                                item.getGlobalId()
-                                            )
-                                            alertUser("Expense deleted!", context)
-                                            showDialog.value = false
-                                            refreshAndOpenGroup(groupId)
-                                        }) {
-                                            Text("Yes")
-                                        }
-                                    },
-                                    dismissButton = {
-                                        TextButton(onClick = { showDialog.value = false }) {
-                                            Text("No")
-                                        }
+                                    append(
+                                        expense.getAmount().toInt().toString()
+                                    )
+
+                                    withStyle(
+                                        style = SpanStyle(
+                                            fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                                            fontStyle = MaterialTheme.typography.bodySmall.fontStyle
+                                        )
+                                    ) {
+                                        append(
+                                            "." + ((expense.getAmount() - expense.getAmount()
+                                                .toInt()) * 100).toInt().toString() + " PLN"
+                                        )
                                     }
+                                }
+
+                                Text(text)
+                            }
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxSize(),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    expense.getDateStamp(),
+                                    style = MaterialTheme.typography.bodySmall,
+                                    color = MaterialTheme.colorScheme.outline,
+                                    modifier = Modifier.padding(bottom = 5.dp, top = 2.dp)
                                 )
                             }
-                        }
-                        Text(
-                            text = "Category: " + item.getCategory(),
-                            fontSize = 14.sp,
+                        },
+                        supportingContent = { Text(expense.getDescription()) },
+                        leadingContent = {
+                            //TODO: place for image placeholder
+                            /*
+                            *  Leave this placeholder if there is no image or image is still loading
+                            *  After image is loaded change this Icon to the actual image
+                            *  On click image should expand to its natural size and displayed to user
+                            * */
+
+                            Icon(EvaIcons.Fill.Image, "Image placeholder")
+
+                        },
+                        trailingContent = {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                val showDialog = remember { mutableStateOf(false) }
+
+                                IconButton(onClick = {
+                                    showDialog.value = true
+                                }) {
+                                    Icon(EvaIcons.Fill.Trash, "Delete expense")
+                                }
+                                if (showDialog.value) {
+                                    AlertDialog(
+                                        onDismissRequest = { showDialog.value = false },
+                                        title = { Text("Confirmation") },
+                                        text = { Text("Are you sure you want to delete this expense?") },
+                                        confirmButton = {
+                                            TextButton(onClick = {
+                                                BackendService(preferences).deleteExpenseByGlobalId(
+                                                    expense.getGlobalId()
+                                                )
+                                                alertUser("Expense deleted!", context)
+                                                showDialog.value = false
+                                                refreshAndOpenGroup(groupId)
+                                            }) {
+                                                Text("Yes")
+                                            }
+                                        },
+                                        dismissButton = {
+                                            TextButton(onClick = { showDialog.value = false }) {
+                                                Text("No")
+                                            }
+                                        }
+                                    )
+                                }
+
+                                CardArrow(expanded.value)
+                            }
+
+
+                        },
+                        modifier = Modifier
+                            .clickable {
+                                expanded.value = !expanded.value
+                            }
+                    )
+
+                    if (expanded.value) {
+                        Column(
                             modifier = Modifier
-                                .padding(horizontal = 10.dp)
-                                .fillMaxWidth()
-                        )
-                        Text(
-                            text = "Date: " + item.getDateStamp(),
-                            fontSize = 14.sp,
-                            modifier = Modifier
-                                .padding(horizontal = 10.dp)
-                                .fillMaxWidth()
-                        )
-                        val payer = item.getUser()
-                        if (payer != null) {
+                                .padding(horizontal = 10.dp, vertical = 5.dp)
+                        ) {
                             Text(
-                                text = "Payed by: " + payer.getUsername(),
+                                text = "Category: " + expense.getCategory(),
                                 fontSize = 14.sp,
                                 modifier = Modifier
                                     .padding(horizontal = 10.dp)
                                     .fillMaxWidth()
                             )
-                        }
-                        val toWhom = expensesByGlobalId[item.getGlobalId()]
-                        if (toWhom != null && toWhom.isNotEmpty()) {
-                            Column(
-                                modifier = Modifier
-                                    .padding(10.dp)
-                                    .background(Color.White)
-                            ) {
+                            val payer = expense.getUser()
+                            if (payer != null) {
                                 Text(
-                                    text = "To:",
+                                    text = "Payed by: " + payer.getUsername(),
                                     fontSize = 14.sp,
                                     modifier = Modifier
                                         .padding(horizontal = 10.dp)
                                         .fillMaxWidth()
                                 )
-                                for (expenseWithUser in toWhom.filter { it.getAmount() <= 0 }) {
-                                    val user = expenseWithUser.getUser()
-                                    if (user != null) {
-                                        Row(
-                                            modifier = Modifier.fillMaxWidth(),
-                                            horizontalArrangement = Arrangement.SpaceBetween
-                                        ) {
-                                            Text(
-                                                text = user.getUsername(),
-                                                fontSize = 14.sp,
-                                                modifier = Modifier
-                                                    .padding(horizontal = 10.dp)
-                                            )
-                                            Text(
-                                                text = "${expenseWithUser.getAmount()} $groupCurrency",
-                                                fontSize = 14.sp,
-                                                modifier = Modifier
-                                                    .padding(horizontal = 10.dp)
-                                            )
+                            }
+
+                            val toWhom = expensesByGlobalId[expense.getGlobalId()]
+                            if (toWhom != null && toWhom.isNotEmpty()) {
+                                Column(
+                                    modifier = Modifier
+                                        .padding(10.dp)
+//                                    .background(Color.White)
+                                ) {
+                                    Text(
+                                        text = "To:",
+                                        fontSize = 14.sp,
+                                        modifier = Modifier
+                                            .padding(horizontal = 10.dp)
+                                            .fillMaxWidth()
+                                    )
+                                    for (expenseWithUser in toWhom.filter { it.getAmount() <= 0 }) {
+                                        val user = expenseWithUser.getUser()
+                                        if (user != null) {
+                                            Row(
+                                                modifier = Modifier.fillMaxWidth(),
+                                                horizontalArrangement = Arrangement.SpaceBetween
+                                            ) {
+                                                Text(
+                                                    text = user.getUsername(),
+                                                    fontSize = 14.sp,
+                                                    modifier = Modifier
+                                                        .padding(horizontal = 10.dp)
+                                                )
+                                                Text(
+                                                    text = "${expenseWithUser.getAmount()} $groupCurrency",
+                                                    fontSize = 14.sp,
+                                                    modifier = Modifier
+                                                        .padding(horizontal = 10.dp)
+                                                )
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
-                        val coroutineScope = rememberCoroutineScope()
-                        val imgBase64 =
-                            BackendService(preferences).getExpensePicture(item.getGlobalId())
-                        val imageBitmap = remember { mutableStateOf<ImageBitmap?>(null) }
-
-                        DisposableEffect(Unit) {
-                            if (imgBase64 != null && imageBitmap.value == null) {
-                                coroutineScope.launch {
-                                    val bitmap = withContext(Dispatchers.IO) {
-                                        BitmapFactory.decodeByteArray(
-                                            imgBase64,
-                                            0,
-                                            imgBase64.size
-                                        ).asImageBitmap()
-                                    }
-                                    imageBitmap.value = bitmap
-                                }
-                            }
-                            onDispose { }
-                        }
-
-                        if (imageBitmap.value != null) {
-                            Image(
-                                bitmap = imageBitmap.value!!,
-                                contentDescription = "Image",
-                                modifier = Modifier
-                                    .padding(horizontal = 10.dp)
-                                    .fillMaxWidth()
-                            )
-                        }
                     }
+                }
+
+                if (filteredData.indexOf(expense) == filteredData.lastIndex) {
+                    Box(
+                        modifier = Modifier
+                            .height(100.dp)
+                    )
                 }
             }
         )
     }
+
 }
