08d724a8984f1d71e3481eb9f118e087db290cf5
Uladzislau Kharlamau <kharlamov-vlad03@mail.ru>
2023-06-14 20:55:51 +0200

    Update expandable List

--- app/src/main/java/com/rebalance/ui/component/main/ExpandableList.kt
+++ app/src/main/java/com/rebalance/ui/component/main/ExpandableList.kt
@@ -1,9 +1,10 @@
 package com.rebalance.ui.component.main
 
 import android.content.Context
-import android.graphics.BitmapFactory
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.*
@@ -12,9 +13,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.rebalance.PreferencesData
@@ -23,6 +26,8 @@ import com.rebalance.backend.service.ExpenseItem
 import com.rebalance.utils.alertUser
 import compose.icons.EvaIcons
 import compose.icons.evaicons.Fill
+import compose.icons.evaicons.fill.Image
+import compose.icons.evaicons.fill.Settings
 import compose.icons.evaicons.fill.Trash
 
 @Composable
@@ -31,11 +36,8 @@ fun ExpandableList(
     preferences: PreferencesData,
     context: Context
 ) {
-    Column(
-        modifier = Modifier
-            .verticalScroll(rememberScrollState())
-    ) {
-        for (item in items) {
+    LazyColumn {
+        items(items = items, itemContent = { item ->
             val expanded = remember { mutableStateOf(false) }
             ListItem(
                 headlineContent = { Text(item.text) },
@@ -58,120 +60,112 @@ fun ExpandableList(
             )
 
             if (expanded.value) {
-                Box(
-                    Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth(), contentAlignment = Alignment.TopStart
+//                Box(
+//                    Modifier
+//                        .padding(16.dp)
+//                        .fillMaxWidth(), contentAlignment = Alignment.TopStart
+//                ) {
+                Column(
                 ) {
-                    Surface() {
-                        Column(
-                            Modifier //TODO: change to LazyColumn
-                                .padding(15.dp)
-                                .fillMaxWidth()
-                        ) {
-                            for (expense in item.expenses) {
-                                Column(
+                    for ((index, expense) in item.expenses.withIndex()) {
+                        ListItem(
+                            headlineContent = {
+                                Row(
                                     modifier = Modifier
-                                        .wrapContentSize()
-                                        .fillMaxWidth()
-                                        .padding(16.dp),
-                                    horizontalAlignment = Alignment.Start
+                                        .fillMaxSize(),
+                                    verticalAlignment = Alignment.Bottom
                                 ) {
-                                    Column(
-                                        modifier = Modifier
-                                            .wrapContentSize()
-                                            .fillMaxWidth()
-                                            .border(1.dp, MaterialTheme.colorScheme.onBackground)
-                                    ) {
-                                        Row(
-                                            modifier = Modifier
-                                                .wrapContentSize()
-                                                .fillMaxWidth()
-                                                .padding(16.dp),
-                                            horizontalArrangement = Arrangement.SpaceBetween
-                                        ) {
-                                            Text(
-                                                expense.getDateStamp(),
-                                                style = MaterialTheme.typography.titleMedium
+
+                                    val text = buildAnnotatedString {
+
+                                        append(
+                                            expense.getAmount().toInt().toString()
+                                        )
+
+                                        withStyle(
+                                            style = SpanStyle(
+                                                fontSize = MaterialTheme.typography.bodySmall.fontSize,
+                                                fontStyle = MaterialTheme.typography.bodySmall.fontStyle
                                             )
-                                            Text(
-                                                expense.getAmount().toString(),
-                                                style = MaterialTheme.typography.titleMedium
-                                            )
-                                        }
-                                        Row(
-                                            modifier = Modifier
-                                                .wrapContentSize()
-                                                .fillMaxWidth()
-                                                .padding(16.dp),
-                                            horizontalArrangement = Arrangement.SpaceBetween
                                         ) {
-                                            Text("Description: " + expense.getDescription())
-                                        }
-                                        val imgBase64 =
-                                            BackendService(preferences).getExpensePicture(expense.getGlobalId())
-                                        if (imgBase64 != null) {
-                                            Row(
-                                                modifier = Modifier
-                                                    .padding(10.dp)
-                                                    .fillMaxWidth(),
-                                                horizontalArrangement = Arrangement.SpaceBetween
-                                            ) {
-
-                                                Image(
-                                                    bitmap = BitmapFactory.decodeByteArray(
-                                                        imgBase64,
-                                                        0,
-                                                        imgBase64.size
-                                                    ).asImageBitmap(),
-                                                    contentDescription = "Image",
-                                                    modifier = Modifier.fillMaxWidth()
-                                                )
-                                            }
-                                        }
-                                        val showDialog = remember { mutableStateOf(false) }
-
-                                        IconButton(onClick = {
-                                            showDialog.value = true
-                                        }) {
-                                            Icon(EvaIcons.Fill.Trash, "Delete expense")
-                                        }
-
-                                        if (showDialog.value) {
-                                            AlertDialog(
-                                                onDismissRequest = { showDialog.value = false },
-                                                title = { Text("Confirmation") },
-                                                text = { Text("Are you sure you want to delete this expense?") },
-                                                confirmButton = {
-                                                    TextButton(onClick = {
-                                                        BackendService(preferences).deleteExpenseByGlobalId(
-                                                            expense.getGlobalId()
-                                                        )
-                                                        alertUser("Expense deleted!", context)
-                                                        showDialog.value = false
-                                                        expanded.value = false
-                                                        //TODO update screen
-                                                    }) {
-                                                        Text("Yes")
-                                                    }
-                                                },
-                                                dismissButton = {
-                                                    TextButton(onClick = {
-                                                        showDialog.value = false
-                                                    }) {
-                                                        Text("No")
-                                                    }
-                                                }
+                                            append(
+                                                "." + ((expense.getAmount() - expense.getAmount()
+                                                    .toInt()) * 100).toInt().toString() + " PLN"
                                             )
                                         }
                                     }
+
+                                    Text(text)
                                 }
-                            }
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxSize(),
+                                    verticalAlignment = Alignment.CenterVertically
+                                ) {
+                                    Text(
+                                        expense.getDateStamp(),
+                                        style = MaterialTheme.typography.bodySmall,
+                                        color = MaterialTheme.colorScheme.outline
+                                    )
+                                }
+                            },
+                            supportingContent = { Text(expense.getDescription()) },
+                            leadingContent = {
+                                //TODO: place for image placeholder
+                                /*
+                                *  Leave this placeholder if there is no image or image is still loading
+                                *  After image is loaded change this Icon to the actual image
+                                *  On click image should expand to its natural size and displayed to user
+                                * */
+
+                                Icon(EvaIcons.Fill.Image, "Image placeholder")
+
+                            },
+                            trailingContent = {
+                                val showDialog = remember { mutableStateOf(false) }
+
+                                IconButton(onClick = {
+                                    showDialog.value = true
+                                }) {
+                                    Icon(EvaIcons.Fill.Trash, "Delete expense")
+                                }
+
+                                if (showDialog.value) {
+                                    AlertDialog(
+                                        onDismissRequest = { showDialog.value = false },
+                                        title = { Text("Confirmation") },
+                                        text = { Text("Are you sure you want to delete this expense?") },
+                                        confirmButton = {
+                                            TextButton(onClick = {
+                                                BackendService(preferences).deleteExpenseByGlobalId(
+                                                    expense.getGlobalId()
+                                                )
+                                                alertUser("Expense deleted!", context)
+                                                showDialog.value = false
+                                                expanded.value = false
+                                                //TODO update screen
+                                            }) {
+                                                Text("Yes")
+                                            }
+                                        },
+                                        dismissButton = {
+                                            TextButton(onClick = {
+                                                showDialog.value = false
+                                            }) {
+                                                Text("No")
+                                            }
+                                        }
+                                    )
+                                }
+                            },
+                        )
+                        if (index != item.expenses.size - 1) {
+                            Divider()
                         }
                     }
                 }
             }
-        }
+        })
     }
 }
 
