eedab3704b41f7253c3c67e443c4acdbff8b3288
DemyDeni <demydeni@gmail.com>
2023-12-04 02:21:05 +0100

    Add parameter isLoading to PrimaryButton on Auth screens to show loading indicator when true

--- app/src/main/java/com/rebalance/ui/component/authentication/PrimaryButton.kt
+++ app/src/main/java/com/rebalance/ui/component/authentication/PrimaryButton.kt
@@ -2,9 +2,11 @@ package com.rebalance.ui.component.authentication
 
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,18 +15,31 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun PrimaryButton(label: String, paddingTop: Dp, onClick: () -> Unit) {
+fun PrimaryButton(
+    label: String,
+    paddingTop: Dp,
+    isLoading: Boolean = false,
+    onClick: () -> Unit
+) {
     Button(
         onClick = onClick,
         modifier = Modifier
             .padding(top = paddingTop)
             .width(180.dp)
             .height(50.dp),
-        shape = RoundedCornerShape(40.dp)
+        shape = RoundedCornerShape(40.dp),
+        enabled = !isLoading
     ) {
-        Text(
-            text = label,
-            fontSize = 18.sp
-        )
+        if (isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .size(35.dp)
+            )
+        } else {
+            Text(
+                text = label,
+                fontSize = 18.sp
+            )
+        }
     }
 }
