76ecfd9439c63ae063dd680bb328b4b8e86a7554
DemyDeni <demydeni@gmail.com>
2023-12-09 16:50:38 +0100

    Remove not used old exceptions

--- app/src/main/java/com/rebalance/backend/exceptions/FailedLoginException.kt
+++
@@ -1,6 +0,0 @@
-package com.rebalance.backend.exceptions
-
-class FailedLoginException: RuntimeException {
-    constructor() : super("Login failed")
-    constructor(message: String) : super(message)
-}
--- app/src/main/java/com/rebalance/backend/exceptions/ServerException.kt
+++
@@ -1,6 +0,0 @@
-package com.rebalance.backend.exceptions
-
-class ServerException : RuntimeException {
-    constructor() : super("Server exception occurred")
-    constructor(message: String) : super(message)
-}
