45be3e9548cc55d5148b98e3bfc604afe4d04ee0
DemyDeni <demydeni@gmail.com>
2023-12-13 01:43:58 +0100

    Remove deleted param from ApiGroupExpenseUserResponse

--- app/src/main/java/com/rebalance/backend/api/dto/response/ApiGroupExpenseUserResponse.kt
+++ app/src/main/java/com/rebalance/backend/api/dto/response/ApiGroupExpenseUserResponse.kt
@@ -3,7 +3,6 @@ package com.rebalance.backend.api.dto.response
 import java.math.BigDecimal
 
 data class ApiGroupExpenseUserResponse(
-    val id: Long,
     val amount: BigDecimal,
     val multiplier: Int,
     val userId: Long
