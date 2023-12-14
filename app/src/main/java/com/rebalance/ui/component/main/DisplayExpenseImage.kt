db298969e8ebfad66e7d6cfa6823be567f621361
Slovjaninsky <nevage7go@gmail.com>
2023-12-14 03:11:53 +0100

    Rework Image composable

--- app/src/main/java/com/rebalance/ui/component/main/DisplayExpenseImage.kt
+++ app/src/main/java/com/rebalance/ui/component/main/DisplayExpenseImage.kt
@@ -3,24 +3,30 @@ package com.rebalance.ui.component.main
 import android.content.Context
 import android.graphics.Bitmap
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asAndroidBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.rebalance.backend.service.BackendService
 import com.rebalance.util.alertUser
 import compose.icons.EvaIcons
 import compose.icons.evaicons.Fill
+import compose.icons.evaicons.fill.Close
 import compose.icons.evaicons.fill.Image
 
 @Composable
@@ -36,62 +42,81 @@ fun DisplayExpenseImage(
     var icon by remember { mutableStateOf<ImageBitmap?>(null) }
     var image by remember { mutableStateOf<ImageBitmap?>(null) }
 
-    LaunchedEffect(showPicture) {
-        if (showPicture) {
-            image = backendService.getImageByExpenseId(expenseId)
-            if (image != null) {
-                icon = Bitmap.createScaledBitmap(
-                    image!!.asAndroidBitmap(), 100, 100, false
-                ).asImageBitmap()
-            }
+    LaunchedEffect(Unit) {
+        image = backendService.getImageByExpenseId(expenseId)
+        if (image != null) {
+            icon = Bitmap.createScaledBitmap(
+                image!!.asAndroidBitmap(), 100, 100, false
+            ).asImageBitmap()
         }
     }
 
     if (icon != null) {
         if (showPicture && image != null) {
-            AlertDialog(
-                onDismissRequest = { onIconClick(false) },
-                title = {
+            Dialog(
+                onDismissRequest = {
+                    onIconClick(false)
+                }
+            ) {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(vertical = 50.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    shadowElevation = 8.dp
+                ) {
                     Box(
-                        modifier = Modifier.clickable(onClick = {
-                            onIconClick(false)
-                        })
+                        modifier = Modifier.fillMaxSize()
                     ) {
                         Image(
                             bitmap = image!!,
                             contentDescription = "Image",
-                            modifier = Modifier.fillMaxWidth()
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .align(Alignment.Center)
                         )
+                        Surface(
+                            shape = CircleShape,
+                            color = Color.Transparent,
+                            modifier = Modifier
+                                .size(50.dp)
+                                .align(Alignment.TopEnd)
+                        ) {
+                            IconButton(
+                                onClick = { onIconClick(false) },
+                            ) {
+                                Icon(
+                                    EvaIcons.Fill.Close,
+                                    contentDescription = "Close",
+                                    tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                                    modifier = Modifier.size(40.dp)
+                                )
+                            }
+                        }
                     }
-                },
-                text = {
-                    Box(
-                        modifier = Modifier.fillMaxWidth(),
-                        contentAlignment = Alignment.Center
-                    )
-                    {
-                        Text(text = "Click again to close")
-                    }
-                },
-                tonalElevation = 20.dp,
-                confirmButton = {}
-            )
+                }
+
+            }
         }
-        IconButton(onClick = {
-            onIconClick(true)
-        }) {
+        IconButton(
+            onClick = {
+                onIconClick(true)
+            }
+        ) {
             Image(
                 bitmap = icon!!,
                 contentDescription = "Expanse image as an icon"
             )
         }
     } else {
-        IconButton(onClick = {
-            alertUser(
-                "No image for this expense",
-                context
-            )
-        }) {
+        IconButton(
+            onClick = {
+                alertUser(
+                    "No image for this expense",
+                    context
+                )
+            }
+        ) {
             Icon(EvaIcons.Fill.Image, "Image placeholder")
         }
     }
