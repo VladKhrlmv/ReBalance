59df07ff8bfc6333db6289b5c689fc835a62cfbe
DemyDeni <demydeni@gmail.com>
2023-12-14 15:25:23 +0100

    Add try catching on all requests

--- app/src/main/java/com/rebalance/backend/api/RequestSender.kt
+++ app/src/main/java/com/rebalance/backend/api/RequestSender.kt
@@ -44,77 +44,86 @@ class RequestSender(
 
     suspend fun sendPost(url: String, body: String): Pair<Int, String> =
         coroutineScope.async(Dispatchers.IO) {
-            var respCode: Int
-            var respBody = ""
+            try {
+                var respCode: Int
+                var respBody = ""
 
-            val request = URL(baseUrl + url)
-            with(request.openConnection() as HttpURLConnection) {
-                requestMethod = "POST"
-                doInput = true
-                doOutput = true
-                setRequestProperty("Content-Type", "application/json; utf-8")
-                setRequestProperty("Accept", "application/json")
-                setRequestProperty("Authorization", "Bearer $token")
-                val outputStreamWriter = OutputStreamWriter(outputStream)
-                outputStreamWriter.write(body)
-                outputStreamWriter.flush()
+                val request = URL(baseUrl + url)
+                with(request.openConnection() as HttpURLConnection) {
+                    requestMethod = "POST"
+                    doInput = true
+                    doOutput = true
+                    setRequestProperty("Content-Type", "application/json; utf-8")
+                    setRequestProperty("Accept", "application/json")
+                    setRequestProperty("Authorization", "Bearer $token")
+                    val outputStreamWriter = OutputStreamWriter(outputStream)
+                    outputStreamWriter.write(body)
+                    outputStreamWriter.flush()
 
-                Log.d("net", "post to $url : $responseCode token $token")
-                respCode = responseCode
-                if (responseCode == 200 || responseCode == 201) {
-                    inputStream.bufferedReader().use {
-                        respBody = it.readText()
+                    Log.d("net", "post to $url : $responseCode token $token")
+                    respCode = responseCode
+                    if (responseCode == 200 || responseCode == 201) {
+                        inputStream.bufferedReader().use {
+                            respBody = it.readText()
+                        }
                     }
                 }
+                return@async Pair(respCode, respBody)
+            } catch (e: Exception) {
+                return@async Pair(503, "")
             }
-
-            return@async Pair(respCode, respBody)
         }.await()
 
     suspend fun sendPut(url: String, body: String): Pair<Int, String> =
         coroutineScope.async(Dispatchers.IO) {
-            var respCode: Int
-            var respBody = ""
+            try {
+                var respCode: Int
+                var respBody = ""
 
-            val request = URL(baseUrl + url)
-            with(request.openConnection() as HttpURLConnection) {
-                requestMethod = "PUT"
-                doInput = true
-                doOutput = true
-                setRequestProperty("Content-Type", "application/json; utf-8")
-                setRequestProperty("Accept", "application/json")
-                setRequestProperty("Authorization", "Bearer $token")
-                val outputStreamWriter = OutputStreamWriter(outputStream)
-                outputStreamWriter.write(body)
-                outputStreamWriter.flush()
+                val request = URL(baseUrl + url)
+                with(request.openConnection() as HttpURLConnection) {
+                    requestMethod = "PUT"
+                    doInput = true
+                    doOutput = true
+                    setRequestProperty("Content-Type", "application/json; utf-8")
+                    setRequestProperty("Accept", "application/json")
+                    setRequestProperty("Authorization", "Bearer $token")
+                    val outputStreamWriter = OutputStreamWriter(outputStream)
+                    outputStreamWriter.write(body)
+                    outputStreamWriter.flush()
 
-                Log.d("net", "put to $url : $responseCode token $token")
-                respCode = responseCode
-                if (responseCode == 200 || responseCode == 201) {
-                    inputStream.bufferedReader().use {
-                        respBody = it.readText()
+                    Log.d("net", "put to $url : $responseCode token $token")
+                    respCode = responseCode
+                    if (responseCode == 200 || responseCode == 201) {
+                        inputStream.bufferedReader().use {
+                            respBody = it.readText()
+                        }
                     }
                 }
+                return@async Pair(respCode, respBody)
+            } catch (e: Exception) {
+                return@async Pair(503, "")
             }
-
-            return@async Pair(respCode, respBody)
         }.await()
 
     suspend fun sendDelete(url: String): Pair<Int, String> =
         coroutineScope.async(Dispatchers.IO) {
-            var respCode: Int
+            try {
+                var respCode: Int
 
-            val request = URL(baseUrl + url)
-            with(request.openConnection() as HttpURLConnection) {
-                requestMethod = "DELETE"
-                setRequestProperty("Content-Type", "application/json; utf-8")
-                setRequestProperty("Accept", "application/json")
-                setRequestProperty("Authorization", "Bearer $token")
+                val request = URL(baseUrl + url)
+                with(request.openConnection() as HttpURLConnection) {
+                    requestMethod = "DELETE"
+                    setRequestProperty("Content-Type", "application/json; utf-8")
+                    setRequestProperty("Accept", "application/json")
+                    setRequestProperty("Authorization", "Bearer $token")
 
-                Log.d("net", "delete to $url : $responseCode token $token")
-                respCode = responseCode
+                    Log.d("net", "delete to $url : $responseCode token $token")
+                    respCode = responseCode
+                }
+                return@async Pair(respCode, "")
+            } catch (e: Exception) {
+                return@async Pair(503, "")
             }
-
-            return@async Pair(respCode, "")
         }.await()
 }
